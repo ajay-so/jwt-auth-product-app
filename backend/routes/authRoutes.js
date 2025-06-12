9a2c5de2e6a0922ab91509463eda8f4a39c53037
Ajay Kumar <ajaykumar89693479@gmail.com>
2025-06-12 08:09:37 +0530

    change folder name

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/auth");
+const { registerUser, loginUser } = require("../Controllers/auth");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
